5aa5ba1c866c34990951bfaac0f1cc3fe350e73f
Roxana Rodriguez-Becker <126708010+roxyrodbeck@users.noreply.github.com>
2023-11-12 13:21:38 -0600

    Update main.js
    A lot of changes based on dev30 answer video.

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 var runnersAge = 40;
-var earlyAdults = (runnersAge >= 18 && raceNumber >= 1000);
+var earlyRegistration = false;
 
-
-if (earlyAdults) {
+if (runnersAge > 18 && earlyRegistration === true) {
     raceNumber += 1000;
-    console.log("You are running at 9:30AM.")
-}  else if (runnersAge < 18) {
+}
+
+console.log(`Runner age is ${runnersAge}`);
+console.log(`Runner registered early: ${earlyRegistration}`);
+
+if (runnersAge > 18 && earlyRegistration === true) {
+    console.log(`You are running at 9:30AM. Bib number: ${raceNumber}`);
+} else if (runnersAge > 18 && earlyRegistration === false) {
+     console.log(`You are running at 11AM, bib number ${raceNumber}`);
+} else if (runnersAge < 18) {
     console.log(`You are running at 12:30pm, bib number ${raceNumber}`);
-} else {
-    console.log(`You are running at 11AM, bib number ${raceNumber}`);
+} else if (runnersAge === 18) {
+    console.log(`Happy birthday!`)
 }
